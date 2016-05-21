90736d3d0955415c1bf7809706fa3c4f197c4ac9
gohlivor <gohlivor@gmail.com>
2016-05-21 13:28:43 -0700

    Commit via Commandline

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+    void SetLiveTile(string textmessage) //you should update this with your own images
+    {
+        UnityEngine.WSA.Tile.main.Update("ms-appx:///Data/StreamingAssets/TanksIcon_150x150.png",
+               "ms-appx:///Data/StreamingAssets/TanksIcon_310x150.png", string.Empty, textmessage);
+    }
+}
